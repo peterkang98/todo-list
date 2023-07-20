f12b95eb75e08c4584ea9107666d1cc007b941b1
강동현 <101819709+peterkang98@users.noreply.github.com>
2023-07-20 21:07:29 +0900

    Created task model class

--- src/main/java/com/peterkang/todolist/model/Task.java
+++ src/main/java/com/peterkang/todolist/model/Task.java
@@ -1,9 +1,28 @@
 package com.peterkang.todolist.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
 @Entity
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@AllArgsConstructor
 public class Task {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String title;
+  private String description;
+  private LocalDate dueDate;
+  private boolean completed;
+
 }
